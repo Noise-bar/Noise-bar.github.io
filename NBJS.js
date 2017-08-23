b3c751c9ad46df03d09d8fc49ea017d7c2cc7717
Oliver Smith <31206313+13smith-oliver@users.noreply.github.com>
2017-08-23 11:50:30 +0100

    Add files via upload

--- NBJS.js
+++ NBJS.js
@@ -1,9 +1,3 @@
-var p1 = document.getElementById("TL");
-var p2 = document.getElementById("L");
-var p3 = document.getElementById("N");
-var p4 = document.getElementById("O");
-var p5 = document.getElementById("VG");
-
 var ctx = new AudioContext();
 var processor = ctx.createScriptProcessor(2048, 1, 1)
 
@@ -20,20 +14,20 @@ function f() {
 
     resetColours()
 
-    if (rms > 65) {p1.style.color = "rgb(255, 0, 0)";}
-    else if (rms > 60 && rms <= 65) { p2.style.color = "rgb(255, 140, 0)"; }
-    else if (rms > 55 && rms <= 60) { p3.style.color = "rgb(255, 255, 0)"; }
-    else if (rms > 50 && rms <= 55) { p4.style.color = "rgb(173, 255, 47)"; }
-    else if (rms < 50) { p5.style.color = "rgb(0, 255, 0)"; }
+    if (rms > 65) { document.getElementById("TL").style.color = "rgb(255, 0, 0)";}
+    else if (rms > 60 && rms <= 65) { document.getElementById("L").style.color = "rgb(255, 140, 0)"; }
+    else if (rms > 55 && rms <= 60) { document.getElementById("N").style.color = "rgb(255, 255, 0)"; }
+    else if (rms > 50 && rms <= 55) { document.getElementById("O").style.color = "rgb(173, 255, 47)"; }
+    else if (rms < 50) { document.getElementById("VG").style.color = "rgb(0, 255, 0)"; }
 
     requestAnimationFrame(f);
 }
 
 function resetColours() {
-    p1.style.color = "rgb(110, 110, 110)";
-    p2.style.color = "rgb(110, 110, 110)";
-    p3.style.color = "rgb(110, 110, 110)";
-    p4.style.color = "rgb(110, 110, 110)";
-    p5.style.color = "rgb(110, 110, 110)";
+    document.getElementById("TL").style.color = "rgb(110, 110, 110)";
+    document.getElementById("L").style.color = "rgb(110, 110, 110)";
+    document.getElementById("N").style.color = "rgb(110, 110, 110)";
+    document.getElementById("O").style.color = "rgb(110, 110, 110)";
+    document.getElementById("VG").style.color = "rgb(110, 110, 110)";
 }
 requestAnimationFrame(f);
