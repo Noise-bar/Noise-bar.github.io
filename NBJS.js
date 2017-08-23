3c0f02ee70a158cbc78572b9df95ee106e9e1a8a
Oliver Smith <31206313+13smith-oliver@users.noreply.github.com>
2017-08-23 16:55:59 +0100

    Add files via upload

--- NBJS.js
+++ NBJS.js
@@ -10,24 +10,24 @@ function f() {
 
         while (i < len) total += Math.abs(input[i++])
         rms = Math.sqrt(total / len)
+
+        resetColours()
+
+        if (rms > 65) { document.getElementById("TL").style.backgroundColor = "rgb(255, 0, 0)"; }
+        else if (rms > 60 && rms <= 65) { document.getElementById("L").style.backgroundColor = "rgb(255, 140, 0)"; }
+        else if (rms > 55 && rms <= 60) { document.getElementById("N").style.backgroundColor = "rgb(255, 255, 0)"; }
+        else if (rms > 50 && rms <= 55) { document.getElementById("O").style.backgroundColor = "rgb(173, 255, 47)"; }
+        else if (rms < 50) { document.getElementById("VG").style.backgroundColor = "rgb(0, 255, 0)"; }
+
+        requestAnimationFrame(f);
     }
-
-    resetColours()
-
-    if (rms > 65) { document.getElementById("TL").style.color = "rgb(255, 0, 0)";}
-    else if (rms > 60 && rms <= 65) { document.getElementById("L").style.color = "rgb(255, 140, 0)"; }
-    else if (rms > 55 && rms <= 60) { document.getElementById("N").style.color = "rgb(255, 255, 0)"; }
-    else if (rms > 50 && rms <= 55) { document.getElementById("O").style.color = "rgb(173, 255, 47)"; }
-    else if (rms < 50) { document.getElementById("VG").style.color = "rgb(0, 255, 0)"; }
-
-    requestAnimationFrame(f);
 }
 
 function resetColours() {
-    document.getElementById("TL").style.color = "rgb(110, 110, 110)";
-    document.getElementById("L").style.color = "rgb(110, 110, 110)";
-    document.getElementById("N").style.color = "rgb(110, 110, 110)";
-    document.getElementById("O").style.color = "rgb(110, 110, 110)";
-    document.getElementById("VG").style.color = "rgb(110, 110, 110)";
+    document.getElementById("TL").style.backgroundColor = "rgb(110, 110, 110)";
+    document.getElementById("L").style.backgroundColor = "rgb(110, 110, 110)";
+    document.getElementById("N").style.backgroundColor = "rgb(110, 110, 110)";
+    document.getElementById("O").style.backgroundColor = "rgb(110, 110, 110)";
+    document.getElementById("VG").style.backgroundColor = "rgb(110, 110, 110)";
 }
 requestAnimationFrame(f);
